69f2d0f3d244aa5c72a06e8e49df39a941ba1f4d
Matt <matt@Matts-MacBook-Pro.local>
2018-07-12 14:37:16 -0700

    Removed comment line from __init__.py

--- website/__init__.py
+++ website/__init__.py
@@ -3,6 +3,5 @@ from flask import Flask
 
 BASE_PATH = os.path.dirname(os.path.realpath(__file__))
 app = Flask(__name__, instance_relative_config=True)
-# app.config.from_pyfile('config.py')
 
 from website import views
