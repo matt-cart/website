33a11e41c0ca9bd3ec6b52df9d1f550aa9a1a122
Matt <matt@Matts-MacBook-Pro.local>
2018-07-13 15:13:28 -0700

    Updated view for blog post page. Metadata now handled more elegantly by passing Post object directly to front end rather than building it entirely in the backend.

--- website/views.py
+++ website/views.py
@@ -1,5 +1,6 @@
 import os
 import re
+import datetime
 import markdown2
 import collections
 from flask import render_template, send_from_directory
@@ -14,11 +15,12 @@ class Tag:
 
 
 class Post:
-    def __init__(self, title, date, tags, content, href):
+    def __init__(self, title, date, tags, summary, content_html, href):
         self.title = title
         self.date = date
         self.tags = tags
-        self.content = content
+        self.summary = summary
+        self.content_html = content_html
         self.href = href
 
 
@@ -29,7 +31,26 @@ def index():
 
 @app.route('/blog')
 def blog_home():
-    return render_template('blog_construction.html')
+    tag_dict = dict()
+    posts = []
+    content_path = os.path.join(app.root_path, 'content')
+    for file in os.listdir(content_path):
+        if not file.endswith('.md'):
+            continue
+        full_path = os.path.join(content_path, file)
+        html = md_to_html(full_path)
+        post_obj = get_post_metadata(html)
+        posts.append(post_obj)
+        for tag in post_obj.tags:
+            if tag not in tag_dict.keys():
+                tag_dict[tag] = 0
+            tag_dict[tag] += 1
+    sorted_tag_dict = collections.OrderedDict()
+    for key in sorted(tag_dict.keys()):
+        sorted_tag_dict[key] = tag_dict[key]
+    sorted_posts = sorted(posts, key=lambda x: datetime.datetime.strptime(x.date, '%Y-%m-%d'), reverse=True)
+    return render_template('blog_home.html', posts=sorted_posts,
+        tag_dict=sorted_tag_dict)
 
 @app.route('/blog/<post_title>')
 def blog_post(post_title):
@@ -37,8 +58,8 @@ def blog_post(post_title):
     with open(md_file, 'rU') as f:
         lines = f.readlines()
     html = md_to_html(md_file)
-    html = format_post(html)
-    return render_template('blog_post.html', post=html)
+    post_obj = format_post(html)
+    return render_template('blog_post.html', post=post_obj)
 
 
 @app.route('/blog/tag/<queried_tag>')
@@ -72,30 +93,14 @@ def md_to_html(md_path):
     return html 
 
 
-def get_post_metadata(html):
-    re_pat = re.compile(r'<p>title: (?P<title>[^\n]*)\sdate: (?P<date>\d{4}-\d{2}-\d{2})\stags: (?P<tags>[^\n^<]*)</p>')
+def format_post(html):
+    re_pat = re.compile(r'<p>title: (?P<title>[^\n]*)\sdate: (?P<date>\d{4}-\d{2}-\d{2})\stags: (?P<tags>[^\n^<]*)\ssummary: (?P<summary>[^\n^<]*)</p>')
     match_obj = re.match(re_pat, html)
     title = match_obj.group('title')
     date = match_obj.group('date')
+    summary = match_obj.group('summary')
     tags = sorted([tag.strip() for tag in match_obj.group('tags').split(',')])
     content_html = re.split(re_pat, html)[-1]
     href = os.path.join('http://mattcarter.co', 'blog', title.lower().replace(' ', '-'))
-    return Post(title, date, tags, content_html, href)
+    return Post(title, date, tags, summary, content_html, href)
 
-
-def format_post(html):
-    post_obj = get_post_metadata(html)
-
-    header_html = '<h1>%s</h1><h5>%s</h5><p>Tags: ' % (post_obj.title, post_obj.date)
-    for tag in post_obj.tags:
-        header_html += '<a class="btn btn-default btn-xs tag-link"'
-        header_html += 'href="http://mattcarter.co/blog/tag/%s">' % tag
-        header_html += '<span class="glyphicon glyphicon-tag"></span> %s</a>' % tag
-    header_html += '</p>'
-    html = header_html + post_obj.content
-    return html
-
-
-@app.route('/resume', methods=['GET', 'POST'])
-def resume():
-    return send_from_directory('static', filename='MatthewCarterResume.pdf')
