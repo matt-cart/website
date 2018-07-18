fe03ebd2914135cee30e29ac54ec1cd46a92c55c
Matt <matt@Matts-MacBook-Pro.local>
2018-07-18 15:25:34 -0700

    Added setup.sh script for setting up Ubuntu environment.

---
+++ setup.sh
@@ -0,0 +1,32 @@
+# Update OS and install Python
+sudo apt-get update
+sudo apt install python-minimal
+sudo apt-get install python-pip
+sudo apt install virtualenv
+
+# Clone repository and set up Python virtual environment
+git clone https://github.com/matt-cart/website.git
+cd website
+virtualenv venv
+. venv/bin/activate
+pip install -r requirements.txt
+
+# Install Node and Yarn, then install CSS/JS resources using Yarn
+curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
+sudo apt-get install -y nodejs
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+sudo apt-get update && sudo apt-get install yarn
+cd website/static
+yarn install
+
+# Install Apache
+sudo apt-get install apache2
+sudo apt-get install libapache2-mod-wsgi
+sudo /etc/init.d/apache2 start
+
+# Create symnbolic link for Apache to find
+sudo ln -sT ~/website /var/www/html/website
+sudo /etc/init.d/apache2 restart
+
+# Move up to website home folder
+cd ../..
